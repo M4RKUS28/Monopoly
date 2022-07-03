c660a7efd442ec8ea35abb2a507fadd682619a08
Maths24 <86494790+Maths24@users.noreply.github.com>
2022-07-03 12:19:14 +0200

    Neue Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,12 +1,14 @@
 package Main;
 
+import GUI.Constants;
 import GUI.Spielfeld;
 
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Spielfeld s = new Spielfeld();
-		System.out.println("Spielfeld gestarteet");
+		Spielfeld spielfeld = new Spielfeld();
+		Constants con = new Constants();
+		System.out.println("Spielfeld gestartet");
 		
 	}
 }
