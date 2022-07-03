a1cd84475f4c99fb066083057df48224c06b4eed
Erliassystems <jonas.hoerter@rieden37.de>
2022-07-03 14:17:06 +0200

    Tauschklasse fertig

--- src/Gamelogic/Tausch.java
+++ src/Gamelogic/Tausch.java
@@ -3,7 +3,6 @@ package Gamelogic;
 public class Tausch {
 	private int playerVon, playerZu, geldVon, geldZu, gefFreiKartenVon, gefFreiKartenZu;
 	private int[] haeuserVon, haeuserZu;
-	private int gefFreiKarte;
 	
 	public Tausch(int playerVon, int playerZu, int geldVon, int geldZu, int gefFreiKartenVon, int gefFreiKartenZu, int[] haeuserVon, int[] haeuserZu) {
 		this.playerVon = playerVon;
@@ -18,5 +17,26 @@ public class Tausch {
 	
 	public int getPlayerVon () {
 		return playerVon;
+	}
+	public int getPlayerZu () {
+		return playerZu;
+	}
+	public int getGeldVon () {
+		return geldVon;
+	}
+	public int getGeldZu () {
+		return geldZu;
+	}
+	public int getGefFreiKartenVon () {
+		return gefFreiKartenVon;
+	}
+	public int getGefFreiKartenZu () {
+		return gefFreiKartenZu;
+	}
+	public int[] getHaeuserVon () {
+		return haeuserVon;
+	}
+	public int[] getHaeuserZu () {
+		return haeuserZu;
 	}
 }
